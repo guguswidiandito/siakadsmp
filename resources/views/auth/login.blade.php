389d283a7777e29c163ffaf2022d99d3c905d222
guguswidiandito <guguswidiandito@gmail.com>
2018-01-11 01:10:00 +0700

    edit login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,12 +10,12 @@
         <link href="{{ asset('/css/app.css')}}" rel="stylesheet">
         <style type="text/css">
         body {
-        padding-top: 40px;
+        padding-top: 150px;
         padding-bottom: 40px;
         background-color: #f5f5f5;
         }
         .form-signin {
-        max-width: 300px;
+        max-width: 350px;
         padding: 19px 29px 29px;
         margin: 0 auto 20px;
         background-color: #fff;
@@ -52,26 +52,20 @@
                 {{ csrf_field() }}
                 <h2 class="form-signin-heading">Please sign in</h2>
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                        @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                        @endif
-                    </div>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
+                    @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                    @endif
                 </div>
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">Password</label>
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control" name="password" required>
-                        @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                        @endif
-                    </div>
+                    <input id="password" type="password" class="form-control" name="password" required placeholder="Password">
+                    @if ($errors->has('password'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                    @endif
                 </div>
                 <button class="btn btn-large btn-primary" type="submit">Sign in</button>
             </form>
